725bc7f0d326a8ddc3c0830e9d9015c41425546d
Alberto <albertojunior848423803@gmail.com>
2025-01-25 18:33:10 +0300

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/go-sql-driver/mysql"
@@ -24,9 +25,20 @@ func main() {
 		log.Fatal(err)
 	}
 
+	initStorage(db)
+
 	server := api.NEWAPIServer(":8080", db)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB : Successfully Connected")
 }
