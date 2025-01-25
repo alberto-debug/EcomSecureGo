66e320afe8b97e14883c13121409e7f5525b411c
Alberto <albertojunior848423803@gmail.com>
2025-01-25 11:02:10 +0300

    changes

--- db/db.go
+++ db/db.go
@@ -2,9 +2,16 @@ package db
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/go-sql-driver/mysql"
 )
 
 func MySQLStorage(cfg mysql.Config) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(db)
+	}
+
+	return db, nil
 }
